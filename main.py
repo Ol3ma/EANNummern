a0bea94364e5728110e2cacf9b7d6577971dece3
BuildTools <unconfigured@null.spigotmc.org>
2024-03-25 13:47:15 +0100

    java class main erstellt

--- main.py
+++
@@ -1,9 +0,0 @@
-import tkinter
-
-
-root = tkinter.Tk()
-
-label1 = tkinter.Label(root, text="Hallo Welt")
-label1.pack()
-
-root.mainloop()
---
+++ src/main/java/ole/mahlstedt/main.java
@@ -0,0 +1,9 @@
+package ole.mahlstedt;
+
+public class main {
+
+public static void main(String[] args){
+System.out.println("hello World");
+
+}
+}
